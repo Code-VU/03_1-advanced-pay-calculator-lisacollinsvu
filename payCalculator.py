c5cbc6a13735611a6dae650f6b4e0b876a289f62
Lisa Collins <Lisa.Collins@vu.com>
2025-01-07 14:14:42 -0600

    Lisa advanced calc with optional

--- payCalculator.py
+++ payCalculator.py
@@ -1,6 +1,21 @@
 def calculatePay():
     # This first line is provided for you
     hrs = input("Enter Hours:")
+    try: isnumber = float(hrs)
+    except: isnumber = False
+    if isnumber == False: print("Error, please enter numeric input")
+    else: 
+        rate = input("Enter Rate:")
+        try: isnumber = float(rate)
+        except: isnumber = False 
+        if isnumber == False: print("Error, please enter numeric input")
+        else:
+            if hrs <= 40: 
+                pay = float(hrs)*float(rate)
+            elif hrs > 40: 
+                overtime = float(hrs) - 40
+                pay = (40*float(rate)) + (overtime*float(rate)*1.5)
+            print("Pay:", pay)
     
     # end assignment
 
